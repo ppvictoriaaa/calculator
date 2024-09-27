3297982054ce73c79a4b3e3e2cfcb85f353d11dd
ppvictoriaaa <pekurvika@gmail.com>
2024-09-27 21:26:40 +0300

    add Div and Mod funcs

--- calculator.cpp
+++ calculator.cpp
@@ -3,15 +3,25 @@
 int Calculator::Add (double a, double b)
 {
 
-	return a + b + 0.55;
+	return a + b;
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    return Add (a, (-b));
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.55;
+    return a * b; 
+}
+
+int Calculator::Div(double a, double b)
+{
+    return a / b;
+}
+
+int Calculator::Mod(double a, double b)
+{
+    return a % b;
 }
--- calculator.h
+++ calculator.h
@@ -9,6 +9,8 @@ class Calculator
         int Sub (double, double);
         int Add (int, int);
         int Mul (double, double);
+        int Div (double, double);
+        int Mod (double, double);
 };
 
 #endif//CALCULATOR_H
